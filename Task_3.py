d2ccc7cbedf13cf07ea2db651a4919a2158cd08d
Сергей Боровиков <BorovikovSergei@MacBook-Air-Maria.local>
2022-07-12 08:43:50 +0300

    rework

--- Task_3.py
+++ Task_3.py
@@ -7,7 +7,7 @@
 def mathematic(number: int):
     sum = 0
     for i in range(1, number + 1):
-        sum = (1 + (i // 1))  ** i 
+        sum = (1 + (1 / i))  ** i 
         print(sum, end=' ')
     
 
